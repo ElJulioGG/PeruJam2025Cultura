7e6e468c933ae07aec651315694a6215f365bf61
TicSide <66962537+TicSide@users.noreply.github.com>
2025-06-07 12:46:14 -0500

    Feat; Adding Some packages and fonts

---
+++ CulturaPeu2025/Assets/Script/UI/FadeTransition.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+public class FadeTransition : MonoBehaviour
+{
+    [Header("Referencia a la imagen UI")]
+    public Image uiImage;
+
+    [Header("Duración del fade")]
+    public float fadeDuration = 1f;
+
+    [Header("GameObject a desactivar")]
+    public GameObject image;
+
+    void Start()
+    {
+        FadeOut();
+
+    }
+
+    public void FadeOut()
+    {
+        uiImage.DOFade(0f, fadeDuration).OnComplete(() =>
+        {
+            image.SetActive(false);
+        });
+    }
+
+    public void FadeIn()
+    {
+        image.SetActive(true);
+        uiImage.DOFade(1f, fadeDuration).OnComplete(() =>
+        {
+            image.SetActive(false); 
+        });
+    }
+
+}
